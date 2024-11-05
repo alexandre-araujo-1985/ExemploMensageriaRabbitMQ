b205a60d30f7c476c8a9ba158e1ae1770ecfd654
alexa <alexa@LAPTOP-UF0HUGSI>
2024-11-05 20:46:25 -0300

    Ajustes no projeto de Worker

---
+++ ExemploMensageriaRabbitMQ.Consumer/Program.cs
@@ -0,0 +1,10 @@
+﻿using ExemploMensageriaRabbitMQ.Consumer.Listeners;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder =  Host.CreateApplicationBuilder(args);
+
+builder.Services.AddHostedService<ClienteListener>();
+
+var host = builder.Build();
+host.Run();
